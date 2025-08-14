e72458b47614f8f109345dfb61631cc40b19e2fc
Sergio García López <109881039+sergiogarcialopez@users.noreply.github.com>
2025-08-14 14:57:13 -0600

    Emparejamiento Firebase
    SCRUM-13 Integración de las librerías de Firebase, configuración del proyecto de Firebase, a través del panel de Firebase en Android Studio. En el manifiesto se anulan las advertencias relacionadas al requisito de versión de Android en dichas librerías

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -39,6 +40,9 @@ android {
 }
 
 dependencies {
+
+    implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
+    implementation("com.google.firebase:firebase-firestore")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
